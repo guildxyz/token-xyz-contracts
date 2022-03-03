f6ab4147e7b101614189102077b4198ec412f975
Tomi_Ohl <ohlslager.tom@gmail.com>
2022-03-03 16:26:02 +0100

    Add Merkle Tree-related scripts and mock ERC20

---
+++ contracts/mock/ERC20MintableBurnable.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../features/deployables/token/ERC20MintableOwned.sol";
+
+/// @title A mintable and burnable ERC20 token
+contract ERC20MintableBurnable is ERC20MintableOwned {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 tokenDecimals,
+        address minter,
+        uint256 initialSupply
+    ) ERC20MintableOwned(name, symbol, tokenDecimals, minter, initialSupply) {}
+
+    /// @notice Burn an amount of tokens from an account
+    function burn(address account, uint256 amount) public onlyOwner {
+        _burn(account, amount);
+    }
+}
