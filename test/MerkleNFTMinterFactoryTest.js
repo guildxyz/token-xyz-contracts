989322e77d95a116a0525d588bd55cb0f5c7e9a5
Tomi_Ohl <ohlslager.tom@gmail.com>
2022-05-11 17:39:24 +0200

    Fix MerkleNFTMinterFactory tests

--- test/MerkleNFTMinterFactoryTest.js
+++ test/MerkleNFTMinterFactoryTest.js
@@ -8,10 +8,8 @@ const ITokenXyz = artifacts.require("ITokenXyz");
 const SimpleFunctionRegistryFeature = artifacts.require("SimpleFunctionRegistryFeature");
 const OwnableFeature = artifacts.require("OwnableFeature");
 const MerkleNFTMinterFactoryFeature = artifacts.require("MerkleNFTMinterFactoryFeature");
-const MerkleNFTMinter = artifacts.require("MerkleNFTMinter");
-const MerkleNFTMinterAutoId = artifacts.require("MerkleNFTMinterAutoId");
-const ERC721 = artifacts.require("ERC721Mintable");
-const ERC721AutoIdBatchMint = artifacts.require("ERC721AutoIdBatchMint");
+const ERC721MerkleDrop = artifacts.require("ERC721MerkleDrop");
+const ERC721BatchMerkleDrop = artifacts.require("ERC721BatchMerkleDrop");
 
 const randomRoot = "0xf7f77ea15719ea30bd2a584962ab273b1116f0e70fe80bbb0b30557d0addb7f3";
 const nftMetadata0 = {
@@ -69,9 +67,9 @@ contract("MerkleNFTMinterFactory", function (accounts) {
   it("creates minter contracts with the right parameters", async function () {
     const minterAddressesAlice = await tokenXyz.getDeployedNFTMinters("Alice");
     const minterAddressesBob = await tokenXyz.getDeployedNFTMinters("Bob");
-    const minterAlice0 = await MerkleNFTMinter.at(minterAddressesAlice[0].contractAddress);
-    const minterAlice1 = await MerkleNFTMinter.at(minterAddressesAlice[1].contractAddress);
-    const minterBob0 = await MerkleNFTMinter.at(minterAddressesBob[0].contractAddress);
+    const minterAlice0 = await ERC721MerkleDrop.at(minterAddressesAlice[0].contractAddress);
+    const minterAlice1 = await ERC721MerkleDrop.at(minterAddressesAlice[1].contractAddress);
+    const minterBob0 = await ERC721MerkleDrop.at(minterAddressesBob[0].contractAddress);
     const blockTime = await time.latest();
     expect(await minterAlice0.merkleRoot()).to.eq(randomRoot);
     expect(await minterAlice1.merkleRoot()).to.eq(randomRoot);
@@ -87,12 +85,9 @@ contract("MerkleNFTMinterFactory", function (accounts) {
   it("passes correct parameters for NFT creation", async function () {
     const minterAddressesAlice = await tokenXyz.getDeployedNFTMinters("Alice");
     const minterAddressesBob = await tokenXyz.getDeployedNFTMinters("Bob");
-    const minterAlice0 = await MerkleNFTMinter.at(minterAddressesAlice[0].contractAddress);
-    const minterAlice1 = await MerkleNFTMinter.at(minterAddressesAlice[1].contractAddress);
-    const minterBob0 = await MerkleNFTMinter.at(minterAddressesBob[0].contractAddress);
-    const nftAlice0 = await ERC721.at(await minterAlice0.token());
-    const nftAlice1 = await ERC721AutoIdBatchMint.at(await minterAlice1.token());
-    const nftBob0 = await ERC721AutoIdBatchMint.at(await minterBob0.token());
+    const nftAlice0 = await ERC721MerkleDrop.at(minterAddressesAlice[0].contractAddress);
+    const nftAlice1 = await ERC721MerkleDrop.at(minterAddressesAlice[1].contractAddress);
+    const nftBob0 = await ERC721MerkleDrop.at(minterAddressesBob[0].contractAddress);
     expect(await nftAlice0.name()).to.eq(nftMetadata0.name);
     expect(await nftAlice0.symbol()).to.eq(nftMetadata0.symbol);
     expect(await nftAlice0.maxSupply()).to.bignumber.eq(nftMetadata0.maxSupply);
@@ -106,23 +101,19 @@ contract("MerkleNFTMinterFactory", function (accounts) {
 
   it("deploys different contracts based on parameter and they are not mintable by anyone but the owner", async function () {
     await tokenXyz.createNFTMinter("Alice", randomRoot, new BN(86400), nftMetadata1, true, wallet0); // specific ID
-    await tokenXyz.createNFTMinter("Alice", randomRoot, new BN(86400), nftMetadata1, false, wallet0); // auto ID
+    await tokenXyz.createNFTMinter("Alice", randomRoot, new BN(86400), nftMetadata1, false, wallet0); // batch
     const minterAddressesAlice = await tokenXyz.getDeployedNFTMinters("Alice");
-    const minterAliceSpecific = await MerkleNFTMinter.at(
+    const nftAliceSpecific = await ERC721MerkleDrop.at(
       minterAddressesAlice[[minterAddressesAlice.length - 2]].contractAddress
     );
-    const minterAliceAuto = await MerkleNFTMinterAutoId.at(
+    const nftAliceAuto = await ERC721BatchMerkleDrop.at(
       minterAddressesAlice[[minterAddressesAlice.length - 1]].contractAddress
     );
-    const addressNftAliceSpecific = await minterAliceSpecific.token();
-    const addressNftAliceAuto = await minterAliceAuto.token();
     // create artifacts with the right and the other interface too
-    const nftAliceSpecific = await ERC721.at(addressNftAliceSpecific);
-    const nftAliceSpecificWrong = await ERC721AutoIdBatchMint.at(addressNftAliceSpecific);
-    const nftAliceAuto = await ERC721AutoIdBatchMint.at(addressNftAliceAuto);
-    const nftAliceAutoWrong = await ERC721.at(addressNftAliceAuto);
-    await expectRevert(nftAliceSpecific.safeMint(wallet0, "0"), "Ownable: caller is not the owner");
-    await expectRevert(nftAliceAuto.safeMint(wallet0), "Ownable: caller is not the owner");
+    const nftAliceSpecificWrong = await ERC721BatchMerkleDrop.at(nftAliceSpecific.address);
+    const nftAliceAutoWrong = await ERC721MerkleDrop.at(nftAliceAuto.address);
+    await expectRevert(nftAliceSpecific.safeMint(wallet0, "0", { from: wallet1 }), "Ownable: caller is not the owner");
+    await expectRevert(nftAliceAuto.safeMint(wallet0, { from: wallet1 }), "Ownable: caller is not the owner");
     await expectRevert.unspecified(nftAliceSpecificWrong.safeMint(wallet0));
     await expectRevert.unspecified(nftAliceAutoWrong.safeMint(wallet0, "1"));
   });
