e71b98745bff1725f6262a62651375e89eb01110
Tomi_Ohl <ohlslager.tom@gmail.com>
2022-05-12 14:40:20 +0200

    Fix: starting tests sometimes failed

--- run-tests.js
+++ run-tests.js
@@ -1,9 +1,9 @@
 const { exec, spawn } = require("child_process");
 
-exec('npx ganache --mnemonic "horn horn horn horn horn horn horn horn horn horn horn horn"', { stdio: "ignore" });
+exec('ganache --mnemonic "horn horn horn horn horn horn horn horn horn horn horn horn"', { stdio: "ignore" });
 
-const truffleArgs = ["truffle", "test", "--migrations_directory", "test"];
+const truffleArgs = ["test", "--migrations_directory", "test"];
 if (process.argv[2] !== undefined) truffleArgs.push(process.argv[2]);
-const truffle = spawn("npx", truffleArgs, { stdio: "inherit" });
+const truffle = spawn("truffle", truffleArgs, { stdio: "inherit" });
 
 truffle.on("exit", () => process.exit());
